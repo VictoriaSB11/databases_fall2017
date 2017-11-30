590093e9c95939ba660411040544988a288c66df
Victoria Spann-Burton <victoria.spann@nyu.edu>
2017-11-29 23:44:05 -0500

    Posting changes and fixed qeuries

--- init.py
+++ init.py
@@ -3,16 +3,17 @@
 from flask import Flask, render_template, request, session, url_for, redirect
 import pymysql.cursors
 from hashlib import sha1
+import time
 
 app = Flask(__name__)
 
 #Configure MySQL
 
 conn = pymysql.connect(host='localhost',
-					   port=3306,
+					   port=8889,
                        user='root',
-                       passwd='',
-                       db='pricosha',
+                       passwd='password',
+                       db='prichosha',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 	
@@ -92,8 +93,10 @@ def registerAuth():
 def home():
 	username = session['username']
 	cursor = conn.cursor();
-	query = 'SELECT Content.timest, content_name, file_path, comment_text FROM Content INNER JOIN Comment ON Content.id = Comment.id WHERE Content.username = %s ORDER BY timest DESC'
-	cursor.execute(query, (username))
+	query = 'SELECT id, username, content_name, file_path, timest\
+	FROM content WHERE username = %s || public = %s || id in \
+	(SELECT id FROM Share, Member WHERE Share.group_name = Member.group_name  && Member.username = %s) ORDER BY timest DESC'
+	cursor.execute(query, (username, True, username))
 	data = cursor.fetchall()
 	cursor.close()
 	return render_template('home.html', username=username, posts=data)
@@ -102,10 +105,10 @@ def home():
 def post():
 	username = session['username']
 	cursor = conn.cursor();
-	file_path = request.files['image_path']
+	file_path = request.form['image_path']
 	content_name = request.form['content_name']
 	public=request.form['optradio']
-	query = 'INSERT INTO Content(username,file_path,content_name,public) VALUES(%s, %s, %s,%s)'
+	query = 'INSERT INTO Content(username, file_path, content_name, public) VALUES(%s, %s, %s, %s)'
 	cursor.execute(query, (username, file_path, content_name, public))
 	conn.commit()
 	cursor.close()
