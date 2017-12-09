5b5fe448357bd321dbcb8283c3bac74bac09e60e
Victoria Spann-Burton <victoria.spann@nyu.edu>
2017-12-09 17:28:16 -0500

    add party/event, add friend, rsvp, add fixed login bug

--- init.py
+++ init.py
@@ -10,10 +10,10 @@ app = Flask(__name__)
 #Configure MySQL
 
 conn = pymysql.connect(host='localhost',
-					   port=3306,
+					   port=8889,
                        user='root',
-                       passwd='',
-                       db='pricosha',
+                       passwd='password',
+                       db='prichosha',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 	
@@ -139,7 +139,7 @@ def home():
 	(SELECT id FROM Share, Member WHERE Share.group_name = Member.group_name  && Member.username = %s) ORDER BY timest DESC'
 	cursor.execute(query1, (username, True, username))
 
-	query2 = 'SELECT timest, content_name, file_path, likes FROM Content WHERE username = %s && public = 1 ORDER BY timest DESC'
+	query2 = 'SELECT timest, content_name, file_path FROM Content WHERE username = %s && public = 1 ORDER BY timest DESC'
 	cursor.execute(query2, (username))
 	data = cursor.fetchall()
 	cursor.close()
@@ -152,23 +152,9 @@ def post():
 	file_path = request.form['image_path']
 	content_name = request.form['content_name']
 	public=request.form['optradio']
-	likes=0
-	query = 'INSERT INTO Content(username, file_path, content_name, public,likes) VALUES(%s, %s, %s, %s,%s)'
-	cursor.execute(query, (username, file_path, content_name, public,likes))
+	query = 'INSERT INTO Content(username, file_path, content_name, public) VALUES(%s, %s, %s, %s,%s)'
+	cursor.execute(query, (username, file_path, content_name, public))
 	conn.commit()
-	cursor.close()
-	return redirect(url_for('home'))
-
-
-@app.route('/likes')
-def likes(content_name):
-	username = session['username']
-	cursor = conn.cursor();
-	query = 'SELECT likes FROM content WHERE username = %s'
-	cursor.execute(query, (username))
-	data = cursor.fetchall()
-	query2 = 'UPDATE content SET likes = likes+1 WHERE username = %s'
-	cursor.execute(query2, (data,username))
 	cursor.close()
 	return redirect(url_for('home'))
 
@@ -177,10 +163,60 @@ def friends():
 	username = session['username']
 	cursor = conn.cursor();
 	query = 'SELECT DISTINCT group_name, username_creator FROM member WHERE username = %s OR username_creator = %s'
-	cursor.execute(query, (username))
+	cursor.execute(query, (username, username))
 	data = cursor.fetchall()
 	cursor.close()
 	return render_template('friends.html', username=username, groups=data)
+
+@app.route('/addFriend')
+def addFriend():
+	username = session['username']
+	cursor = conn.cursor();
+	query = 'SELECT DISTINCT group_name FROM FriendGroup WHERE username = %s'
+	cursor.execute(query, (username))
+	data = cursor.fetchall()
+	cursor.close()
+	return render_template('addFriend.html', username=username, groups=data)
+
+@app.route('/addFriendtoGroup', methods=['GET','POST'])
+def addFriendtoGroup():
+	username = session['username']
+	selectedGroup = request.form.get('select_group')
+	memFirst = request.form['memfname']
+	memLast = request.form['memlname']
+	memUsername = request.form['memUsername']
+	cursor = conn.cursor();
+
+	#check that there is one person with this name
+	queryFindMemUsername = "SELECT username FROM Person	WHERE first_name = %s AND last_name = %s"
+	cursor.execute(queryFindMemUsername, (memFirst, memLast))
+	friendUsername = cursor.fetchall()
+
+	if( friendUsername > 1):
+		error = "More than one person with that name: %s. Please provide the username as well" % (friendUsername)
+		return redirect(url_for('addFriend', error=error))
+
+	queryAlreadyInGroup = "SELECT * FROM member WHERE username = %s AND group_name = %s"
+	cursor.execute(queryAlreadyInGroup, (friendUsername, selectedGroup))
+	data = cursor.fetchall()
+
+	if(data):
+		error = "This person is already in the group"
+		return redirect(url_for('addFriend', error=error))
+
+	if(memUsername):
+		queryAddFriend = "INSERT INTO Member(username, group_name, username_creator) VALUES(%s, %s, %s)"
+		cursor.execute(queryAddFriend, (memUsername, selectedGroup, username))
+		conn.commit()
+		cursor.close()
+		return redirect(url_for('addFriend'))
+
+	else:
+		queryAddFriend = "INSERT INTO Member(username, group_name, username_creator) VALUES(%s, %s, %s)"
+		cursor.execute(queryAddFriend, (friendUsername, selectedGroup, username))
+		conn.commit()
+		cursor.close()
+		return redirect(url_for('addFriend'))
 
 
 @app.route('/tagandshare')
@@ -224,6 +260,122 @@ def addFriendGroup():
 	conn.commit()
 	cursor.close()
 	return redirect(url_for('friends'))
+
+@app.route('/parties')
+def parties():
+	username = session['username']
+	cursor = conn.cursor();
+	query = 'SELECT DISTINCT party.party_name, username_host, datetime, going FROM invites JOIN party ON invites.party_name = party.party_name WHERE invites.username = %s OR username_host = %s'
+	cursor.execute(query, (username, username))
+	data = cursor.fetchall()
+	cursor.close()
+	return render_template('parties.html', username=username, parties=data)
+
+@app.route('/createParty', methods=['GET','POST'])
+def createParty():
+	username = session['username']
+	cursor = conn.cursor();
+	partyName = request.form['partyName']
+	partyLocation = request.form['partyLocation']
+	partyDatetime = request.form['partyDatetime']
+	partyDescription = request.form['partyDescription']
+	 
+	queryNewParty = "INSERT INTO Party (party_name, username, description, datetime, location) VALUES(%s, %s, %s, %s, %s)"
+	cursor.execute(queryNewParty, (partyName, username, partyDescription, partyDatetime, partyLocation))
+	
+	queryMeAsHost = "INSERT INTO Invites (username, party_name, username_host, going) VALUES(%s, %s, %s, %s)"
+	cursor.execute(queryMeAsHost, (username, partyName, username, True))
+
+	queryPeople = "SELECT username, first_name, last_name FROM Person"
+	cursor.execute(queryPeople)
+	dataPeople = cursor.fetchall()
+
+	conn.commit()
+	cursor.close()
+	return redirect(url_for('parties'))
+
+@app.route('/invite')
+def invite():
+	username = session['username']
+	cursor = conn.cursor();
+
+	queryMyParties = "SELECT party_name FROM Party WHERE username = %s"
+	cursor.execute(queryMyParties, (username))
+	data = cursor.fetchall()
+
+	conn.commit()
+	cursor.close()
+	return render_template('invite.html', parties=data)
+
+@app.route('/invitePeople', methods=['GET','POST'])
+def invitePeople():
+	username = session['username']
+	selectedParty = request.form.get('select_party')
+	inviteeFirst = request.form['invfname']
+	inviteeLast = request.form['invlname']
+	cursor = conn.cursor();
+
+	#ensure person exists and get their username
+	queryFindMemUsername = "SELECT username FROM Person	WHERE first_name = %s AND last_name = %s"
+	cursor.execute(queryFindMemUsername, (inviteeFirst, inviteeLast))
+	inviteeUsername = cursor.fetchone().get('username')
+
+	queryAlreadyInvited = "SELECT * FROM Invites WHERE username = %s AND party_name = %s"
+	cursor.execute(queryAlreadyInvited, (inviteeUsername, selectedParty))
+	data = cursor.fetchall()
+
+	if(data):
+		error = "This person was already invited"
+		return redirect(url_for('invite', error=error))
+	else:
+		querySendInvite = "INSERT INTO Invites(username, party_name, username_host, going) VALUES(%s, %s, %s, %s)"
+		cursor.execute(querySendInvite, (inviteeUsername, selectedParty, username, False))
+		conn.commit()
+		cursor.close()
+		return redirect(url_for('invite'))
+
+@app.route('/viewInvites')
+def viewInvites():
+	username = session['username']
+	cursor = conn.cursor();
+
+	queryPartyGoing = "SELECT username_host, Party.party_name, description, location, going, datetime FROM Invites JOIN Party ON Invites.party_name = Party.party_name WHERE Invites.username = %s AND going = %s"
+	cursor.execute(queryPartyGoing, (username, True))
+	goingData = cursor.fetchall()
+
+	queryPartyNotGoing = "SELECT username_host, Party.party_name, description, location, going, datetime FROM Invites JOIN Party ON Invites.party_name = Party.party_name WHERE Invites.username = %s AND going = %s"
+	cursor.execute(queryPartyNotGoing, (username, False))
+	notGoingData = cursor.fetchall()
+
+	conn.commit()
+	cursor.close()
+	return render_template('rsvp.html', going=goingData, notGoing=notGoingData)
+
+@app.route('/rsvp', methods=['GET','POST'])
+def rsvp():
+	username = session['username']
+	selectedParty = request.form.get('select_party')
+	cursor = conn.cursor();
+
+	#ensure person exists and get their username
+	queryUpdateRSVP = "UPDATE Invites SET going = %s WHERE username = %s AND party_name = %s"
+	cursor.execute(queryUpdateRSVP, (True, username, selectedParty))
+	conn.commit()
+	cursor.close()
+	return redirect(url_for('rsvp'))
+
+@app.route('/unrsvp', methods=['GET','POST'])
+def unrsvp():
+	username = session['username']
+	selectedParty = request.form.get('select_party')
+	cursor = conn.cursor();
+
+	#ensure person exists and get their username
+	queryUpdateRSVP = "UPDATE Invites SET going = %s WHERE username = %s AND party_name = %s"
+	cursor.execute(queryUpdateRSVP, (False, username, selectedParty))
+	conn.commit()
+	cursor.close()
+	return redirect(url_for('rsvp'))
 
 @app.route('/profile')
 def backProfile():
